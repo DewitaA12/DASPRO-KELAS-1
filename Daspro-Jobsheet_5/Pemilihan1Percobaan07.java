10c38b1c037ca94b6a3d5447b2733365d0cd8c10
DewitaA12 <xsclthzn@gmail.com>
2023-10-26 16:43:00 +0700

    Ternary Operator

--- Daspro-Jobsheet_5/Pemilihan1Percobaan07.java
+++ Daspro-Jobsheet_5/Pemilihan1Percobaan07.java
@@ -4,10 +4,7 @@ public class Pemilihan1Percobaan07 {
     Scanner input07 = new Scanner(System.in);
     System.out.println("Masukkan Angka :");
     int angka = input07.nextInt();
-    if (angka %2 == 0){
-        System.out.println("Angka " + angka + " Bilangan GENAP");
-    }else {
-        System.out.println("Angka " + angka + " Bilangan GANJIL");
-    }
+    String bilangan = (angka % 2 == 0 ) ? "GANJIL" : "GENAP" ;
+    System.out.println(bilangan);
     }
 }
