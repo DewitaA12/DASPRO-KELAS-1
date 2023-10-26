1fa6e5edf499568746c566925a21cabe579e1917
DewitaA12 <xsclthzn@gmail.com>
2023-10-26 15:46:14 +0700

    9/12/2023

---
+++ Daspro-Jobsheet_3/ContohVariabel07.java
@@ -0,0 +1,14 @@
+public class ContohVariabel07 {
+    public static void main(String[] args){
+String HobySaya = "Menggambar";
+boolean isPandai = true;
+char jenisKelamin = 'P';
+byte umur = 19;
+double ipk = 4.00, tinggi = 1.63;
+System.out.println("Hoby apa ? " + HobySaya);
+System.out.println("Apakah pandai ? " +  isPandai);
+System.out.println("Jenis kelamin " +  jenisKelamin);
+System.out.println("Umurku saat ini: "+ umur);
+System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", ipk, tinggi));
+}
+}
