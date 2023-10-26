3f08028016c7f0054565bef736bb9e3fa8d8e9d3
DewitaA12 <xsclthzn@gmail.com>
2023-10-26 15:10:24 +0700

    9/5/2023

---
+++ Daspro-Jobsheet_1/Latihan07part2.java
@@ -0,0 +1,10 @@
+public class Latihan07part2 {
+    public static void main(String[]args){
+        System.out.println("*****IDENTITAS DIRI*****");
+        System.out.println("Nama    : Dewita Anggraini");
+        System.out.println("Absen   : 07");
+        System.out.println("Kelas   : 1D");
+        System.out.println("Prodi   : Teknik Informatika");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
