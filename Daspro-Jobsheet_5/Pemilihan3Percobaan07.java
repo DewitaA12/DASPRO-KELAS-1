1987fca6a072dff9bf82209de2e752e495818e60
DewitaA12 <xsclthzn@gmail.com>
2023-10-26 17:12:01 +0700

    9/25/2023

---
+++ Daspro-Jobsheet_5/Pemilihan3Percobaan07.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Pemilihan3Percobaan07 {
+    public static void main(String[] args) {
+        Scanner input07 = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        char operator;
+        System.out.println("Masukkan angka pertama: ");
+        angka1 = input07.nextDouble();
+        System.out.println("Masukkan angka kedua: ");
+        angka2 = input07.nextDouble();
+        System.out.println("Masukkan operator(+-*/): ");
+        operator = input07.next().charAt(0);
+        switch (operator) {
+            case '+' :
+            hasil = angka1 + angka2;
+            System.out.println(angka1 + " + " + angka2 + " = " + hasil);
+            break;
+        case '-':
+            hasil = angka1 - angka2;
+            System.out.println(angka1 + " - " + angka2 + " = " + hasil);
+            break;
+        case '*':
+            hasil = angka1 * angka2;
+            System.out.println(angka1 + " * " + angka2 + " = " + hasil);
+            break;
+        case '/':
+            hasil = angka1 / angka2;
+            System.out.println(angka1 + " / " + angka2 + " = " + hasil);
+            break;
+        }
+
+    }
+}
