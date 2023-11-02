28dbcff8742a4120030dd768c31c90cb02ee68b7
DewitaA12 <xsclthzn@gmail.com>
2023-11-03 00:08:42 +0700

    perbaikan kesalahan

--- Daspro-Jobsheet_7/PenjumlahanDeret07.java
+++ Daspro-Jobsheet_7/PenjumlahanDeret07.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 public class PenjumlahanDeret07 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
+        Scanner sc7 = new Scanner(System.in);
+        int total = 0;
+        System.out.println("Masukkan nilai n :");
+        int n = sc7.nextInt();
+        for (int i = n; i>=1; i--) {
+                total += i;
+        }
+        sc7.close();
+        System.out.println("Hasil penjumlahan deret n sampai 1 adalah :" + total);
     }
 }
