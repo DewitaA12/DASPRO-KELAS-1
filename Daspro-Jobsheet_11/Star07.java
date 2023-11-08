52e6b8e315f581210b30bd36dd3e2dd87445c2b3
DewitaA12 <xsclthzn@gmail.com>
2023-11-08 13:07:55 +0700

    star

---
+++ Daspro-Jobsheet_11/Star07.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Star07 {
+    public static void main(String[] args){
+    Scanner sc7 = new Scanner(System.in);
+    System.out.print("Masukkan nilai N = ");
+    int N = sc7.nextInt();
+    for (int i=1; i<=N; i--){
+        System.out.print("*");
+    }
+    sc7.close();
+    }
+}
