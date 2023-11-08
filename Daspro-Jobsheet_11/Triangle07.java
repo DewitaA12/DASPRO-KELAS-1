0951775140843133c7af91f322dfb6bbd00a4e3d
DewitaA12 <xsclthzn@gmail.com>
2023-11-08 14:10:07 +0700

    Percobaan 3

---
+++ Daspro-Jobsheet_11/Triangle07.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Triangle07 {
+    public static void main(String[] args){
+    Scanner sc7 = new Scanner(System.in); 
+    System.out.print("Masukkan nilai N = "); 
+    int N = sc7.nextInt(); 
+    int i = 0; 
+    while(i <= N) { 
+    int j = 0; 
+    while(j < i) { 
+        System.out.print("*"); 
+        j++; 
+    } 
+    i++; 
+    } 
+    sc7.close();
+    }
+}
