1ce5d1ee69a6c0494d978da8b1008ff4ff41e086
DewitaA12 <xsclthzn@gmail.com>
2023-11-11 23:57:50 +0700

    M >> foreach

--- Daspro-Jobsheet_11/NestedLoop_2341720100.java
+++ Daspro-Jobsheet_11/NestedLoop_2341720100.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
+
 public class NestedLoop_2341720100 {
     public static void main(String[] args) {
-    Scanner sc7 = new Scanner(System.in);
-    Double[][] array = new Double[5][7];
-    for(int i = 0; i<array.length; i++) {
-    System.out.println("Kata ke-" + i);
-    for (int j = 0; j < array.length; j++) {
-        System.out.print("Hari ke-" + (j+i) + ": ");
-        array[i][j] = sc7.nextDouble();
-    }
-    System.out.println();
-    }for (int i = 0; i < array.length; i++) { 
-        System.out.print("Kota ke-" + (i + 1) + ": "); 
-        for (int j = 0; j < array[0].length; j++) { 
-        System.out.print(array[i][j] + " "); 
-        } 
-        System.out.println(); 
-       }
-    sc7.close();       
+        Scanner sc7 = new Scanner(System.in);
+        Double[][] array = new Double[5][7];
+
+        // Mengisi array
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print("Hari ke-" + (j + i) + ": ");
+                array[i][j] = sc7.nextDouble();
+            }
+            System.out.println();
+        }
+
+        // Menampilkan array menggunakan foreach
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (Double suhu : array[i]) {
+                System.out.print(suhu + " ");
+            }
+            System.out.println();
+        }
+
+        sc7.close();
     }
 }
