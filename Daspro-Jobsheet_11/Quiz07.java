6181f4c7b8742d36a47717782e3ebb02948b586a
DewitaA12 <xsclthzn@gmail.com>
2023-11-11 23:37:02 +0700

    Modified <&&>

--- Daspro-Jobsheet_11/Quiz07.java
+++ Daspro-Jobsheet_11/Quiz07.java
@@ -12,10 +12,17 @@ do{
         System.out.print("Tebak angka (1-10): "); 
         int answer = input.nextInt(); 
         input.nextLine(); 
-        success = (answer == number); 
+        if (answer == number){
+            success = true;
+        } else if (answer < number){
+            System.out.println("Your guess is less than the answer, try again!");
+        }else {
+            System.out.println("Your guess is more than the answer, try again!");
+        }
     } while(!success); 
     System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?"); 
     menu = input.nextLine().charAt(0); 
 } while(menu =='y' || menu =='Y');
+input.close();
 }
 }
