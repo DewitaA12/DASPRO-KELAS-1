b39a7248d5b1ef02c409f6a2105ccbe294fb7ee6
DewitaA12 <xsclthzn@gmail.com>
2023-11-13 23:05:38 +0700

    insert comment

--- Daspro-Jobsheet_11/StarTriangle07.java
+++ Daspro-Jobsheet_11/StarTriangle07.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 public class StarTriangle07 {
     public static void main(String[] args) {
         Scanner sc7 = new Scanner(System.in);
+
+// Masukkan Nilai N
         System.out.print("Masukkan nilai N (min 5) : ");
         int N = sc7.nextInt();
         int i = 0;
         while(i <= N) { 
+// Membuat urutan dari banyak ke sedikit
             int j = N; 
             while(j >= i) { 
+// Mencetak baris
                 System.out.print("*"); 
                 j--; 
             } 
