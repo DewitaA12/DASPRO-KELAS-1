72c8c150a9ae7f12f0f8cc49e5f91bc3f518de64
DewitaA12 <xsclthzn@gmail.com>
2023-11-13 23:48:28 +0700

    fix

--- Daspro-Jobsheet_6/Latihan1BilTerbesar07.java
+++ Daspro-Jobsheet_6/Latihan1BilTerbesar07.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
+
 public class Latihan1BilTerbesar07 {
     public static void main(String[] args) {
         Scanner inp07 = new Scanner(System.in);
         int a, b, c, terbesar;
-        System.out.println("Masukkan nilai a =");
+
+        System.out.print("Masukkan nilai a = ");
         a = inp07.nextInt();
-        System.out.println("Masukkan nilai b =");
+        System.out.print("Masukkan nilai b = ");
         b = inp07.nextInt();
-        System.out.println("Masukkan nilai c =");
+        System.out.print("Masukkan nilai c = ");
         c = inp07.nextInt();
+
+        // Inisialisasi terbesar dengan nilai a
+        terbesar = a;
+
+        // Membandingkan nilai b dan c dengan terbesar
         terbesar = (b > terbesar) ? b : terbesar;
         terbesar = (c > terbesar) ? c : terbesar;
+
         System.out.println("Bilangan terbesar: " + terbesar);
 
-    inp07.close();
+        inp07.close();
     }
 }
