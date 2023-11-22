4f5cf5b6e4376d0c16135156ed5fc1fdbe992e18
DewitaA12 <xsclthzn@gmail.com>
2023-11-22 15:24:06 +0700

    fungsi

--- Daspro-Jobsheet_13/percobaan607.java
+++ Daspro-Jobsheet_13/percobaan607.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 public class percobaan607 {
-    public static void main(String[] args){
-        Scanner inp07 = new Scanner(System.in);
-
+    //Fungsi Hitung Luas
+    static int hitungLuas (int pjg, int lb) {
+        int Luas = pjg*lb;
+        return Luas;
+    }
+    //Fungsi Hitung Volume
+    static int hitungVolume (int tinggi, int a, int b) {
+        int volume = hitungLuas (a,b)*tinggi;
+        return volume;
+    }
+    //Fungsi main
+    public static void main(String[] args) {
+        Scanner inp07 = new Scanner (System.in);
         int p, l, t, L, vol;
-
         System.out.println("Masukkan panjang");
         p = inp07.nextInt();
         System.out.println("Masukkan lebar");
         l = inp07.nextInt();
         System.out.println("Masukkan tinggi");
-        t = inp07.nextInt();
+        t=inp07.nextInt();
 
-        L = p*l;
-        System.out.println("Luas Persegi panjang adalah " + L);
-
-        vol = p*l*t;
-        System.out.println("Volume balok adalah " + vol);
-        inp07.close();
+        L=hitungLuas(p,l);
+        System.out.println("Luas Persegi Panjang adalah " + L);
+        vol = hitungVolume(t,p,l);
+        System.out.println("Volume Balok adalah " + vol);
+    inp07.close();
     }
 
-}
+    }
+
