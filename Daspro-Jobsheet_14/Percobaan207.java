d384e3dbe42720bc748b545bf24520b881914f25
DewitaA12 <xsclthzn@gmail.com>
2023-11-29 13:29:36 +0700

    Modified

--- Daspro-Jobsheet_14/Percobaan207.java
+++ Daspro-Jobsheet_14/Percobaan207.java
@@ -4,8 +4,14 @@ public class Percobaan207 {
 //Fungsi Pangkat Rekursif
 public static int hitungPangkat(int x, int y) {
     if (y == 0) {
+        System.out.print("x" + "1");
         return (1);
     }else {
+        if (y>1){
+            System.out.print(x + "x");
+        }else {
+            System.out.print(x);
+        }
         return (x * hitungPangkat(x, y-1));
     }
 }
@@ -18,7 +24,10 @@ public static void main (String[] args){
     System.out.print("Pangkat: ");
     int pangkat = sc7.nextInt();
 
-    System.out.println(hitungPangkat(bil, pangkat));
+    System.out.print(bil + "^" + pangkat + " = ");
+        int hasil = hitungPangkat(bil, pangkat);
+        System.out.print(" = " + hasil);
+
         sc7.close();
 }
 }
