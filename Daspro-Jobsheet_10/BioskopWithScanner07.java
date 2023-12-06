1842634328c1bf7e87e9d2bc7a6958ffc6dacae9
DewitaA12 <xsclthzn@gmail.com>
2023-12-06 09:37:45 +0700

    add menu

--- Daspro-Jobsheet_10/BioskopWithScanner07.java
+++ Daspro-Jobsheet_10/BioskopWithScanner07.java
@@ -4,27 +4,50 @@ public class BioskopWithScanner07 {
     public static void main(String[] args) {
         Scanner sc7 = new Scanner(System.in);
         int baris, kolom;
-        String nama, next;
+        String nama;
         String[][] penonton = new String[4][2];
 
         while (true) {
-            System.out.print("Masukkan nama : ");
-            nama = sc7.nextLine();
-            System.out.print("Masukkan baris : ");
-            baris = sc7.nextInt();
-            System.out.print("Masukkan kolom : ");
-            kolom = sc7.nextInt();
-            sc7.nextLine();
+            System.out.println("Menu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1/2/3): ");
+            int pilihan = sc7.nextInt();
+            sc7.nextLine(); 
 
-            penonton[baris - 1][kolom - 1] = nama;
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan nama : ");
+                    nama = sc7.nextLine();
+                    System.out.print("Masukkan baris : ");
+                    baris = sc7.nextInt();
+                    System.out.print("Masukkan kolom : ");
+                    kolom = sc7.nextInt();
+                    sc7.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc7.nextLine();
+                    penonton[baris - 1][kolom - 1] = nama;
+                    break;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                case 2:
+                    System.out.println("Daftar Penonton:");
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            if (penonton[i][j] != null) {
+                                System.out.println("Baris " + (i + 1) + ", Kolom " + (j + 1) + ": " + penonton[i][j]);
+                            }
+                        }
+                    }
+                    break;
+
+                case 3:
+                    sc7.close();
+                    return;
+
+                default:
+                    System.out.println("Pilihan tidak valid.");
+                    break;
             }
         }
-    sc7.close();
     }
 }
